d68d9a56a67c09e2354cf6789e1b830f4fb18d15
David <davidcj31@gmail.com>
2020-09-20 14:06:39 -0600

    Header arreglado

--- Proyecto1/resources/views/layouts/header.blade.php
+++ Proyecto1/resources/views/layouts/header.blade.php
@@ -1,43 +1,11 @@
 <header>
 
-<!-- ****** Bara del header ************ -->
+    <!-- ****** Bara del header ************ -->
     <nav class="navbar navbar-expand-lg navbar-dark default-color">
 
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                    <ul class="navbar-nav nav-flex-icons" id="redes">
-                <li class="nav-item">
-                <x-jet-responsive-nav-link class="nav-link" href="/user/profile" :active="request()->routeIs('profile.show')">
-                    {{ __('Profile') }}
-                </x-jet-responsive-nav-link>
-                </li>
-                <li class="nav-item">
-                    <!-- Authentication -->
-                    <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                        <x-jet-responsive-nav-link class="nav-link" href="{{ route('logout') }}"
-                            onclick="event.preventDefault();
-                            this.closest('form').submit();">
-                            {{ __('Logout') }}
-                        </x-jet-responsive-nav-link>
-                </form>
-                </li>
-            </ul>
-
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                        @endif
-                    @endif
-                </div>
-            @endif
-
         <a class="navbar-brand" href="#"><strong>UNA</strong></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -55,7 +23,40 @@
                     <a class="nav-link" href="#">Opinions</a>
                 </li>
             </ul>
+            <ul class="navbar-nav navbar-rigth">
+            @if (Route::has('login'))
+            @auth
+            <li class="nav-item">
+                <x-jet-responsive-nav-link class="nav-link" href="/user/profile"
+                        :active="request()->routeIs('profile.show')">
+                        {{ __('Profile') }}
+                </x-jet-responsive-nav-link>
+            </li>
+            <li class="nav-item">
+            <form method="POST" action="{{ route('logout') }}">
+            @csrf
+
+                        <x-jet-responsive-nav-link class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                            this.closest('form').submit();">
+                            {{ __('Logout') }}
+                        </x-jet-responsive-nav-link>
+                    </form>
+            </li>
+            @else
+            <li class="nav-item">
+                <a href="{{ route('login') }}" class="nav-link">Login</a>
+            </li>
+            @if (Route::has('register'))
+            <li class="nav-item">
+                <a href="{{ route('register') }}" class="nav-link">Register</a>
+            </li>
+            @endif
+            @endif
+            @endif
+            </ul>
         </div>
+
+
     </nav>
 
 </header>
