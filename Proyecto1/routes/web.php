9da46367a08da4c99c20ab1b676812705cef26d0
kevin_Artavia <kevinartavia29@gmail.com>
2020-09-20 00:03:07 -0600

    Primer Commit

--- Proyecto1/routes/web.php
+++ Proyecto1/routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::get('/informe-mensual', function () {
+    return view('informe-mensual');
+});
