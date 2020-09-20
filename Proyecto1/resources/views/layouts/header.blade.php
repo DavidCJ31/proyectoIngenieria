cb24eb19de93028685206a9d9a91dc0ae0f5e247
David <davidcj31@gmail.com>
2020-09-20 03:24:00 -0600

    20/9/2020 Final

--- Proyecto1/resources/views/layouts/header.blade.php
+++ Proyecto1/resources/views/layouts/header.blade.php
@@ -2,26 +2,11 @@
 
 <!-- ****** Bara del header ************ -->
     <nav class="navbar navbar-expand-lg navbar-dark default-color">
-        <a class="navbar-brand" href="#"><strong>UNA</strong></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Opinions</a>
-                </li>
-            </ul>
-            <ul class="navbar-nav nav-flex-icons" id="redes">
+
+            @if (Route::has('login'))
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @auth
+                    <ul class="navbar-nav nav-flex-icons" id="redes">
                 <li class="nav-item">
                 <x-jet-responsive-nav-link class="nav-link" href="/user/profile" :active="request()->routeIs('profile.show')">
                     {{ __('Profile') }}
@@ -39,10 +24,35 @@
                         </x-jet-responsive-nav-link>
                 </form>
                 </li>
+            </ul>
+
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                        @endif
+                    @endif
+                </div>
+            @endif
+
+        <a class="navbar-brand" href="#"><strong>UNA</strong></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
                 <li class="nav-item">
-                    <x-jet-responsive-nav-link href="/user/profile" :active="request()->routeIs('profile.show')">
-                        <img class="h-8 w-8 rounded-full object-cover" style="width:30px; border-radius:50%;" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
-                    </x-jet-responsive-nav-link>
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Opinions</a>
                 </li>
             </ul>
         </div>
