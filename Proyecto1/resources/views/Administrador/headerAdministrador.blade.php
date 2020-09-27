90b888d9e078dde9f58b07eb1851696ff30ff527
Mario <mariosiua@hotmail.com>
2020-09-27 12:09:59 -0600

    Loggin por modulos

---
+++ Proyecto1/resources/views/Administrador/headerAdministrador.blade.php
@@ -0,0 +1,69 @@
+<header>
+
+    <!-- ****** Bara del header ************ -->
+    <nav class="navbar navbar-expand-lg navbar-dark default-color">
+
+        <a class="navbar-brand" href="#"><strong>UNA</strong></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/">Inicio<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/informe-mensual">Informe-mensual</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/formulario">Formulario</a>
+                </li>
+                <li class="nav-item">
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle btn-navbar" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Opciones
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="#">Fomulario</a>
+                            <a class="dropdown-item" href="#">Informe mensual</a>
+                            <a class="dropdown-item" href="/usuarios">Tabla usuarios</a>
+                            <a class="dropdown-item" href="/estudiante">Tabla estudiantes</a>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+            <ul class="navbar-nav navbar-rigth">
+                @if (Route::has('login'))
+                @auth
+                <li class="nav-item">
+                    <a class="nav-link" href="/user/profile" :active="request()->routeIs('profile.show')">
+                        {{ __('Profile') }}
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+                        <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                            this.closest('form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+                    </form>
+                </li>
+                @else
+                <li class="nav-item">
+                    <a href="{{ route('login') }}" class="nav-link">Login</a>
+                </li>
+                @if (Route::has('register'))
+                <li class="nav-item">
+                    <a href="{{ route('register') }}" class="nav-link">Register</a>
+                </li>
+                @endif
+                @endif
+                @endif
+            </ul>
+        </div>
+
+    </nav>
+
+</header>
--- Proyecto1/routes/web.php
+++ Proyecto1/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\PersonaController;
 use App\Http\Controllers\EstudianteController;
 use App\Models\User;
@@ -16,6 +17,25 @@ use App\Models\estudientes;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/logged_in', function(){
+    $rol = Auth::user()->rol;
+    if($rol == 1){//Administrador
+        return view('Administrador/dashboardAdministrador');
+    }
+    if($rol == 2){//Asesor
+        return view('Asesor/dashboardAsesor');
+    }
+    if($rol == 3){//Estudiante
+        return view('Estudiante/dashboardEstudiante');
+    }
+    if($rol == 4){//Tutor
+        return view('Tutor/dashboardTutor');
+    }
+    else{
+        return view('auth/login');
+    }
+});
 
 Route::get('/', function () {
     return view('welcome');
