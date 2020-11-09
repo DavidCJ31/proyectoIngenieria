5b9239fae2e1690f8b2fd5e6eb594d2e8d2607a9
David <davidcj31@gmail.com>
2020-11-08 18:57:57 -0600

    Ingles - español

--- Proyecto1/resources/views/auth/register.blade.php
+++ Proyecto1/resources/views/auth/register.blade.php
@@ -51,21 +51,21 @@
 
 
             <div class="mt-4">
-                <x-jet-label value="{{ __('Password') }}" />
+                <x-jet-label value="{{ __('Contraseña') }}" />
                 <x-jet-input class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
             </div>
 
             <div class="mt-4">
-                <x-jet-label value="{{ __('Confirm Password') }}" />
+                <x-jet-label value="{{ __('Confirmar contraseña') }}" />
                 <x-jet-input class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
+                    {{ __('Ya esta registrado?') }}
                 </a>
                 <x-jet-button class="ml-4">
-                    {{ __('Register') }}
+                    {{ __('Registrase') }}
                 </x-jet-button>
             </div>
         </form>
